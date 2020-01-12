3c9e81961267230acd8ed9b233e6ea450b36a11a
takusan23 <takumu1200@gmail.com>
2020-01-12 18:50:53 +0900

    undefindになる場合がある？直した。

--- scroll.js
+++ scroll.js
@@ -1,4 +1,4 @@
 window.onload = function () {
     //falseをtrueにすると上辺に合わせる？
-    document.getElementsByClassName('MainContainer is-miniBannerVisible')[0].scrollIntoView(false)
+    document.getElementsByClassName('MainContainer-player')[0].scrollIntoView(false)
 }
